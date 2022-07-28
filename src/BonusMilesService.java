153457a1b2e7de51b4a349675bc18d5a2936be69
Maria Mikhno <mmikhno89@gmail.com>
2022-07-28 19:45:08 +0300

    First commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate (int cost) {
+
+        int oneMile = 20; // руб для одной бонусной мили
+        int result;
+
+        result = cost/oneMile;
+        return result;
+    }
+}
