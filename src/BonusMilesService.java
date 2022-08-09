304a936e18bcf2bb783c8ee8b26fe83af9817106
Maria Mikhno <mmikhno89@gmail.com>
2022-08-09 18:59:02 +0300

    Reformatted code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,11 @@
 public class BonusMilesService {
 
-    public int calculate (int cost) {
+    public int calculate(int cost) {
 
         int oneMile = 20; // руб для одной бонусной мили
         int result;
 
-        result = cost/oneMile;
+        result = cost / oneMile;
         return result;
     }
 }
